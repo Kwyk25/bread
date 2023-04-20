70849d285cf844621077043cd812e1b733786dd8
Kyle <kylev006@gmail.com>
2023-04-19 21:09:23 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -11,14 +11,19 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
 
+
+
 // MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public')) 
 
+
 // Breads
 const breadsController = require('./controllers/breads_controllers')
+const breads = require('./controllers/breads_controllers')
 app.use('/breads', breadsController)
   
 // 404 Page
--- views/index.jsx
+++ views/index.jsx
@@ -4,6 +4,10 @@ const Default = require('./layouts/default')
 function Index ({breads, title}) {
     return (
         <Default>
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
+
   <h2>Index Page</h2>
   {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
@@ -21,8 +25,6 @@ function Index ({breads, title}) {
 }
   </ul>
 </Default>
-
-      
     )
 }
 
