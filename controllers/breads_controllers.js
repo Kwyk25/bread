28b3642fccf8f05f1c9c31c87608e320dd38512d
Kyle <kylev006@gmail.com>
2023-04-19 20:18:31 -0700

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -4,14 +4,24 @@ const Bread = require('../models/breads.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
 })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
 })
 
 
 module.exports = breads
-
--- server.js
+++ server.js
@@ -10,11 +10,22 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
-  
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public')) 
+
 // Breads
 const breadsController = require('./controllers/breads_controllers')
 app.use('/breads', breadsController)
   
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 
 // LISTEN
 app.listen(PORT, () => {
